b740e87faf02ab2e0b1dffb1bd6345725a9865c4
kobel <kobelei335@gmail.com>
2024-04-24 19:45:34 -0700

    PORT CHECK

--- src/main/java/org/kobe/xbot/Server/Main.java
+++ src/main/java/org/kobe/xbot/Server/Main.java
@@ -4,13 +4,18 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         if (args.length > 0) {
             try {
                 int port = Integer.parseInt(args[0]);
-                new XTables(port);
+                if (port < 0 || port > 65535) {
+                    logger.severe("Error: The specified port '" + args[0] + "' is outside the specified range of valid port values.");
+                } else {
+                    new XTables(port);
+                }
             } catch (NumberFormatException e) {
-                logger.severe ("Error: The specified port '" + args[0] + "' is not a valid integer.");
+                logger.severe("Error: The specified port '" + args[0] + "' is not a valid integer.");
             }
         } else {
             logger.info("No port number provided. Default port 1735 is being used.");
