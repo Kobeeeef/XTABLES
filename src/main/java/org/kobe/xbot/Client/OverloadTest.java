357da08f7f1871610d16d51b4ffb6ec28e025cac
kobel <kobelei335@gmail.com>
2024-05-22 19:25:45 -0700

    resolver

--- src/main/java/org/kobe/xbot/Client/OverloadTest.java
+++ src/main/java/org/kobe/xbot/Client/OverloadTest.java
@@ -2,6 +2,7 @@ package org.kobe.xbot.Client;
 
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+import org.kobe.xbot.Utilities.ResponseStatus;
 import org.kobe.xbot.Utilities.Utilities;
 import org.kobe.xbot.Utilities.VideoStreamResponse;
 
@@ -14,7 +15,14 @@ public class OverloadTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         // Initialize a new client with address and port
         XTablesClient client = new XTablesClient("XTablesService", 5, false);
+        long time = System.nanoTime();
+        int i = 0;
+        while(i < 500) {
+            i++;
+            ResponseStatus info = client.putString("ok", "1").complete();
 
+        }
+        System.out.println((System.nanoTime() - time) / 1e6);
 
     }
 
