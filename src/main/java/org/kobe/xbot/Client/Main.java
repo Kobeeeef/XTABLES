3d033b69fae1ef5bfa5af3d93be1d05f0f31625a
kobel <kobelei335@gmail.com>
2024-05-30 19:07:29 -0700

    yur

--- src/main/java/org/kobe/xbot/Client/Main.java
+++ src/main/java/org/kobe/xbot/Client/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Initialize a new client with address and port
-        XTablesClient client = new XTablesClient(SERVER_ADDRESS, SERVER_PORT, 1, true);
+        XTablesClient client = new XTablesClient(SERVER_ADDRESS, SERVER_PORT, 3, true);
         // Thread blocks until connection is successful
 
         // Get raw JSON from server
