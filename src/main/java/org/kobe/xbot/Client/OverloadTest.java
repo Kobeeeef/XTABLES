ad40664af742660b528c6cdcbe45f096530eadae
kobel <kobelei335@gmail.com>
2024-06-01 18:54:06 -0700

    optimized to the MAX.

--- src/main/java/org/kobe/xbot/Client/OverloadTest.java
+++ src/main/java/org/kobe/xbot/Client/OverloadTest.java
@@ -5,6 +5,8 @@ import org.kobe.xbot.Utilities.ResponseInfo;
 import org.kobe.xbot.Utilities.ResponseStatus;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OverloadTest {
     private static final String SERVER_ADDRESS = "localhost"; // Server address
@@ -13,14 +15,20 @@ public class OverloadTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         // Initialize a new client with address and port
         XTablesClient client = new XTablesClient();
-
-        while (true) {
-
+        int i = 0;
+        long time = System.nanoTime();
+        List<Double> times = new ArrayList<>();
+        while (i<1000000) {
+            i++;
             try {
+                long start = System.nanoTime();
                 client.executePutInteger("ok", 1);
+                times.add((System.nanoTime() - start) / 1e6);
             } catch (Exception ignored) {}
         }
-
+        System.out.println("Average Time (1M Updates): " + times.stream().mapToDouble(Double::doubleValue )
+                .average().orElse(Double.NaN) + " ms");
+        System.out.println("Total Time (1M Updates): " + (System.nanoTime() - time)/1e6 + " ms");
 
 
     }
--- src/main/java/org/kobe/xbot/Client/XTablesClient.java
+++ src/main/java/org/kobe/xbot/Client/XTablesClient.java
@@ -599,6 +599,16 @@ public class XTablesClient {
         Utilities.validateKey(key, true);
         client.sendMessageRaw("IGNORED:PUT " + key + " " + value);
     }
+    public <T> void executePutArray(String key, List<T> value) {
+        Utilities.validateKey(key, true);
+        String parsedValue = gson.toJson(value);
+        client.sendMessageRaw("IGNORED:PUT " + key + " " + parsedValue);
+    }
+    public void executePutObject(String key, Object value) {
+        Utilities.validateKey(key, true);
+        String parsedValue = gson.toJson(value);
+        client.sendMessageRaw("IGNORED:PUT " + key + " " + parsedValue);
+    }
     public RequestAction<VideoStreamResponse> registerImageStreamServer(String name) {
         Utilities.validateName(name, true);
         return new RequestAction<>(client, new ResponseInfo(null, MethodType.REGISTER_VIDEO_STREAM, name).parsed(), VideoStreamResponse.class) {
