014aff7c8b63b397267d0580891bd88a711cdcbd
kobel <kobelei335@gmail.com>
2024-10-25 22:33:13 -0700

    fix for larger buffer

--- src/main/python/tests.py
+++ src/main/python/tests.py
@@ -1,8 +1,39 @@
+import base64
 import logging
+import time
+
+import cv2
 from XTablesClient import XTablesClient
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    client = XTablesClient(useZeroMQ=True, server_port=1735)
+    client = XTablesClient(useZeroMQ=True)
     while True:
         client.push_frame("ok", "oko")
+
+# if __name__ == "__main__":
+#     logging.basicConfig(level=logging.INFO)
+#     client = XTablesClient(useZeroMQ=True, server_port=1735)
+#     while True:
+#         client.executePutString("ok", "oko")
+
+# if __name__ == "__main__":
+#     logging.basicConfig(level=logging.INFO)
+#     client = XTablesClient(useZeroMQ=True, server_port=1735)
+#     cap = cv2.VideoCapture(0)
+#
+#     while cap.isOpened():
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+#
+#         _, buffer = cv2.imencode('.jpg', frame)
+#         encoded_frame = base64.b64encode(buffer).decode('utf-8')
+#         client.push_frame("image", encoded_frame)
+#
+#         cv2.imshow("Sending Frame", frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#
+#     cap.release()
+#     cv2.destroyAllWindows()
