39f60c8ea131a2d93b63fbae8e0ba302b63a2f0a
kobel <kobelei335@gmail.com>
2024-11-15 21:52:46 -0800

    JAR TESTWDADWAW

--- src/main/java/org/kobe/xbot/Tests/OverloadTest.java
+++ src/main/java/org/kobe/xbot/Tests/OverloadTest.java
@@ -17,8 +17,6 @@ public class OverloadTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         // Initialize a new client with address and port
         XTablesClient client = new org.kobe.xbot.ClientLite.XTablesClient(1735, true, 10, false);
-        List<Integer> a = new ArrayList<>();
-        a.add(123);
         while(true) {
             client.executePutDouble("test", Math.random());
 
