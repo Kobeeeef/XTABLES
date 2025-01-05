92bbceaf7b3a19fd75ee0ffb7f04252b0d48a287
kobel <kobelei335@gmail.com>
2025-01-04 22:11:36 -0800

    type sheet JAR

--- src/main/java/org/kobe/xbot/JClient/XTablesClient.java
+++ src/main/java/org/kobe/xbot/JClient/XTablesClient.java
@@ -71,8 +71,9 @@ public class XTablesClient {
     // These variables are unique to each instance of the class.
     // =============================================================
     private String XTABLES_CLIENT_VERSION =
-            "XTABLES Jero Client v2.0.0 | Build Date: 1/3/2025";
-    private String ip;
+            "XTABLES Jero Client v2.0.0 | Build Date: 1/4/2025";
+
+    private final String ip;
     private final int requestSocketPort;
 
     /**
