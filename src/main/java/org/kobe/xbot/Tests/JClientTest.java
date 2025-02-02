4eb78fec2f0a8d132029a190ab27f121eb87291d
Agent Smith <488matrix@gmail.com>
2025-02-02 12:08:16 -0800

    changing up, default af on

--- src/main/java/org/kobe/xbot/Tests/JClientTest.java
+++ src/main/java/org/kobe/xbot/Tests/JClientTest.java
@@ -1,19 +1,27 @@
 package org.kobe.xbot.Tests;
 
 import org.kobe.xbot.JClient.XTablesClient;
+import org.kobe.xbot.Utilities.Entities.XTableValues;
 import org.kobe.xbot.Utilities.XTablesByteUtils;
+
+import java.util.List;
 
 public class JClientTest {
     public static void main(String[] args) throws InterruptedException {
         XTablesClient xTablesClient = new XTablesClient();
-        xTablesClient.subscribe((xTableUpdate -> {
-           System.out.println(xTableUpdate.getKey() + ": " + XTablesByteUtils.convertXTableUpdateToJsonString(xTableUpdate));
+        xTablesClient.subscribe("target_waypoints", (xTableUpdate -> {
+           System.out.println(xTableUpdate.getKey());
         }));
-        xTablesClient._getXTablesDataProto().getDataMap().forEach((key, value) -> {
-            System.out.println(key + ": " + value);
-        });
 
-        Thread.sleep(100000000);
+        while (true) {
+            List<XTableValues.Coordinate> coordinates = xTablesClient.getCoordinates("target_waypoints");
+            for (XTableValues.Coordinate coordinate : coordinates) {
+                System.out.println(coordinate);
+            }
+        }
+
+
+//        Thread.sleep(100000000);
 
 
     }
