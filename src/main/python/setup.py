ae54f7e413f78a14442021817454154bd757eefe
Agent Smith <488matrix@gmail.com>
2025-02-25 00:44:19 -0800

    up[date zero conf

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="XTablesClient",
-    version="5.7.0",
+    version="5.7.1",
     description="Team 488 XTABLES Python",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
