91e5cfae08fabd3264f04d44b80bccb30a937e48
Agent Smith <488matrix@gmail.com>
2025-02-25 11:19:51 -0800

    Adding more detailed stuff

--- src/main/java/org/kobe/xbot/JClient/XTablesClient.java
+++ src/main/java/org/kobe/xbot/JClient/XTablesClient.java
@@ -54,7 +54,7 @@ public class XTablesClient extends QueuedRequests implements Subscriptions {
     // Instance Variables
     // These variables are unique to each instance of the class.
     // =============================================================
-    private String XTABLES_CLIENT_VERSION = "XTABLES Jero Client v5.3.1 | Build Date: 2/21/2025";
+    private String XTABLES_CLIENT_VERSION = "XTABLES Jero Client v5.3.3 | Build Date: 2/25/2025";
 
     private final String ip;
     private final XTablesSocketMonitor socketMonitor;
--- src/main/java/org/kobe/xbot/Utilities/Entities/VisionCoprocessor.java
+++ src/main/java/org/kobe/xbot/Utilities/Entities/VisionCoprocessor.java
@@ -1,22 +1,26 @@
 package org.kobe.xbot.Utilities.Entities;
 
 public enum VisionCoprocessor {
-    ORIN1("orin-nano-1"),
-    ORIN2("orin-nano-2"),
-    ORIN3("orin-nano-3"),
-    ORANGE_PI_FRONT_RIGHT("photonvisionfrontright"),
-    ORANGE_PI_FRONT_LEFT("photonvisionfrontleft"),
-    ORANGE_PI_REAR_RIGHT("photonvisionrearright"),
-    ORANGE_PI_REAR_LEFT("photonvisionrearleft"),
-    ORANGE_PI_BACK("photonvisionback"),
-    LOCALHOST("localhost");
+    ORIN1("orin-nano-1", true),
+    ORIN2("orin-nano-2", true),
+    ORIN3_DHCP("orin-nano-3", true),
+    ORIN3_STATIC("10.4.88.7", false),
+    ORIN3_XCASTER("orin-3", true),
+    ORANGE_PI_FRONT_RIGHT("photonvisionfrontright", true),
+    ORANGE_PI_FRONT_LEFT("photonvisionfrontleft", true),
+    ORANGE_PI_REAR_RIGHT("photonvisionrearright", true),
+    ORANGE_PI_REAR_LEFT("photonvisionrearleft", true),
+    ORANGE_PI_BACK("photonvisionback", true),
+    LOCALHOST("localhost", true);
     private final String hostname;
+    private final boolean isHostname;
 
-    VisionCoprocessor(String hostname) {
+    VisionCoprocessor(String hostname, boolean isHostname) {
         this.hostname = hostname;
+        this.isHostname = true;
     }
 
     public String getQualifiedHostname() {
-        return this.equals(LOCALHOST) ? hostname : hostname + ".local";
+        return this.isHostname ? this.equals(LOCALHOST) ? hostname : hostname + ".local": hostname;
     }
 }
