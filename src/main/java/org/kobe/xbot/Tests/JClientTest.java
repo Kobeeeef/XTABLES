c14e94d8c090391cea4f8b0cf8b8bfd7c267ec6f
kobel <kobelei335@gmail.com>
2026-01-06 15:45:01 -0800

    Update JClientTest main to send rotating Pose2d

--- src/main/java/org/kobe/xbot/Tests/JClientTest.java
+++ src/main/java/org/kobe/xbot/Tests/JClientTest.java
@@ -25,9 +25,11 @@ public class JClientTest {
         coordinates.add(XTableValues.Coordinate.newBuilder()
                 .setX(Math.random())
                 .setY(3).build());
+        int degrees = 0;
         while (true) {
-
-            client.putCoordinates("target_waypoints", coordinates);
+            degrees -= 5;
+            client.putPose2d("PoseSubsystem.RobotPose", new Pose2d(Math.random() * 50,Math.random() * 30 ,new Rotation2d(Units.degreesToRadians(degrees))));
+            Thread.sleep(10);
         }
 //       Thread.sleep(1000000);
 
